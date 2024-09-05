1b96fb83bacbd946a1142f4ee03b78ebde6faee9
Wanglanean <156498929+Wanglanean@users.noreply.github.com>
2024-09-05 11:38:58 +0800

    Delete collocation_for_antoconvolution 2/choosec.m

--- collocation_for_antoconvolution 2/choosec.m
+++
@@ -1,6 +0,0 @@
-function [c_min,c_max]=choosec(C9,C10,M_line,Kd,normU,m,eta,eps2,delta)
-b1=(C10*eta+sqrt(C10^2*eta^2+4*eps2*C9))/(2*eps2);
-b2=(sqrt(32*m^3)*M_line*Kd*normU)/sqrt(delta);
-c_min=max(b1,b2)+1;
-c_max=eta*c_min;
-end
